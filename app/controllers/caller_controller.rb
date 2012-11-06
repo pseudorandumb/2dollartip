d94b02b322aff6b79fc72409260ef490a04d680c
Kyle Patterson <toodlestech@gmail.com>
2012-11-05 22:47:12 -0800

    remove frenchy :(

--- app/controllers/caller_controller.rb
+++ app/controllers/caller_controller.rb
@@ -8,13 +8,10 @@ class CallerController < ActionController::Base
     else
       resp = '<?xml version="1.0" encoding="UTF-8"?>
               <Response>
-                <Say voice="woman" language="fr">Chapeau!</Say>
-
                 <Gather timeout="10" finishOnKey="*">
                     <Say>Press one to listen to a message from the last caller and record a message for the next caller
                     Press two to play the song of the week. Press three to listen to a poem.</Say>
                 </Gather>
-
               </Response>'
     end
     render xml: resp
