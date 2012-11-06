3f8e0f821d095dc8a260b8d893ea74910900bda9
Kyle Patterson <toodlestech@gmail.com>
2012-11-06 02:03:06 -0800

    dat missing end

--- app/controllers/audio_controller.rb
+++ app/controllers/audio_controller.rb
@@ -1,8 +1,10 @@
 class AudioController < ActionController::Base
-def create 
-# RecordingUrl  the URL of the recorded audio
-# RecordingDuration the duration of the recorded audio (in seconds)
-# Digits  the key (if any) pressed to end the recording or 'hangup' if the caller hung up
-  Audio.create(url: params['RecordingUrl'], duration: params['RecordingDuration'])
-  render xml: "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Say voice=\"woman\" language=\"en\">Thank You!</Say></Response>"
+  def create 
+  # RecordingUrl  the URL of the recorded audio
+  # RecordingDuration the duration of the recorded audio (in seconds)
+  # Digits  the key (if any) pressed to end the recording or 'hangup' if the caller hung up
+    Audio.create(url: params['RecordingUrl'], duration: params['RecordingDuration'])
+    resp = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Say voice=\"woman\" language=\"en\">Thank You!</Say></Response>"
+    render xml: resp
+  end
 end
