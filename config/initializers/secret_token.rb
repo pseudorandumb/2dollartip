84b0e706cfd2f33b9703534b023ad193efc1c35f
Kyle Patterson <toodlestech@gmail.com>
2012-11-05 22:10:18 -0800

    init code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twodollartip::Application.config.secret_token = 'c1e3f63d6c22eabf610f4b013577363c3476f0b7d56e9aad1662481b0102b91be9f954f41f013ecd75de61fa1ba1cc3d91a1b0b878a37f7b53ba181e0d3a1df0'
