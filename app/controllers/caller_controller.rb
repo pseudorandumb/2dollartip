94b21adb90f11f9316ce5e1ca1af1630af931347
Kyle Patterson <toodlestech@gmail.com>
2012-11-06 00:09:10 -0800

    made a whoopsiessss

--- app/controllers/caller_controller.rb
+++ app/controllers/caller_controller.rb
@@ -1,6 +1,6 @@
 class CallerController < ActionController::Base
   def index
-    resp = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Say voice=\"woman\" language=\"en\">Fuck off! #{params['Digits']}</Say></Response>"
+    resp = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Gather timeout=\"10\" finishOnKey=\"*\"><Say>Press one to listen to a message from the last caller and record a message for the next caller press two to play the song of the week. Press three to listen to a poem.</Say></Gather></Response>"
     render xml: resp
   end
 
