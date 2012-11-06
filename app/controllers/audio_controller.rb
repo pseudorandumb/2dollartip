36af4293a77a975f7f870b7c6c8d8356f2b5070c
Kyle Patterson <toodlestech@gmail.com>
2012-11-06 01:55:52 -0800

    added response after audio recording finishes

--- app/controllers/audio_controller.rb
+++ app/controllers/audio_controller.rb
@@ -4,5 +4,5 @@ def create
 # RecordingDuration the duration of the recorded audio (in seconds)
 # Digits  the key (if any) pressed to end the recording or 'hangup' if the caller hung up
   Audio.create(url: params['RecordingUrl'], duration: params['RecordingDuration'])
-end
+  render xml: "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Say voice=\"woman\" language=\"en\">Thank You!</Say></Response>"
 end
