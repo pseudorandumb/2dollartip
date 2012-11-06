a94b268c87871a8900c52ad7b8f8ada9873786e9
Kyle Patterson <toodlestech@gmail.com>
2012-11-05 23:54:35 -0800

    test1

--- app/controllers/caller_controller.rb
+++ app/controllers/caller_controller.rb
@@ -1,11 +1,11 @@
 class CallerController < ActionController::Base
   def index
     resp = ""
-    if params['Digits']
-      resp = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Say voice=\"woman\" language=\"en\">Fuck off! #{params['Digits']]}</Say></Response>"
-    else
+    #if params['Digits']
+    #  resp = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Say voice=\"woman\" language=\"en\">Fuck off! #{params['Digits']]}</Say></Response>"
+    #else
       resp = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Gather timeout=\"10\" finishOnKey=\"*\"><Say>Press one to listen to a message from the last caller and record a message for the next caller press two to play the song of the week. Press three to listen to a poem.</Say></Gather></Response>"
-    end
+    #end
     render xml: resp
   end
 end
